bf11ad361258fb855acbb7efab07847ab8221e7a
codewithdinesh <pr760677@gmail.com>
2023-10-08 23:13:29 +0530

    Fix:Deployment Conguration

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export',
-  basePath: '/gh-pages-test',
+  // output: 'export',
+  // basePath: '/gh-pages-test',
 }
 
 module.exports = nextConfig
